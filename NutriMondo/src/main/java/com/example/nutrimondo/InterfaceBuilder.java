10bb11f75341e123a35d499cd17bc11c5c071693
sensorario <sensorario@gmail.com>
2013-11-15 01:11:46 +0100

    Ops

---
+++ NutriMondo/src/main/java/com/example/nutrimondo/InterfaceBuilder.java
@@ -0,0 +1,174 @@
+package com.example.nutrimondo;
+
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.FrameLayout;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.TimePicker;
+
+public class InterfaceBuilder {
+
+    MainActivity _mainActivity;
+    FrameLayout _frameLayout;
+
+    int width = 440;
+    int leftMargin = 20;
+    int top = 0;
+    int lineHeightTextView = 40;
+    int lineHeightButton = 80;
+    int lineHeightSpinner = 80;
+    int lineHeightTimePicker = 220;
+
+    int multiSpinnerCount = 1;
+    ArrayAdapter<CharSequence> _multiSpinnerAdapter;
+
+    private int getNewMultiSpinnerId() {
+        multiSpinnerCount++;
+        return multiSpinnerCount;
+    }
+
+    InterfaceBuilder(
+            MainActivity mainActivity,
+            FrameLayout frameLayout
+    ) {
+        _mainActivity = mainActivity;
+        _frameLayout = frameLayout;
+    }
+
+    public void addTextView(
+            String text
+    ) {
+        FrameLayout.LayoutParams layoutParams;
+
+        layoutParams = new FrameLayout.LayoutParams(
+                width,
+                lineHeightTextView
+        );
+        layoutParams.leftMargin = leftMargin;
+        layoutParams.topMargin = getNewTop(
+                lineHeightTextView
+        );
+
+        TextView textView = new TextView(_mainActivity);
+        textView.setText(text);
+        textView.setLayoutParams(layoutParams);
+
+        _frameLayout.addView(textView);
+    }
+
+    public void addMultiSpinner(ArrayAdapter<CharSequence> adapter) {
+        addMultiSpinnerSpinner(getNewMultiSpinnerId(), adapter);
+        addMultiSpinnerButton("+");
+    }
+
+    private void addMultiSpinnerSpinner(int id, ArrayAdapter<CharSequence> adapter) {
+        _multiSpinnerAdapter = adapter;
+
+        FrameLayout.LayoutParams layoutParams;
+        layoutParams = new FrameLayout.LayoutParams(
+                width,
+                lineHeightSpinner
+        );
+        layoutParams.leftMargin = leftMargin;
+        layoutParams.topMargin = getNewTop(lineHeightSpinner);
+
+        Spinner spinner = new Spinner(_mainActivity);
+        spinner.setId(id);
+        spinner.setAdapter(adapter);
+        spinner.setLayoutParams(layoutParams);
+
+        _frameLayout.addView(spinner);
+    }
+
+    private void addMultiSpinnerButton(String text) {
+        FrameLayout.LayoutParams layoutParams;
+        layoutParams = new FrameLayout.LayoutParams(
+                width,
+                lineHeightButton
+        );
+        layoutParams.leftMargin = leftMargin;
+        layoutParams.topMargin = getNewTop(lineHeightButton);
+
+        final Button button = new Button(_mainActivity);
+        button.setText(text);
+        button.setLayoutParams(layoutParams);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                top -= lineHeightSpinner;
+                top -= lineHeightSpinner;
+                addMultiSpinnerSpinner(getNewMultiSpinnerId(), _multiSpinnerAdapter);
+                moveMultiSpinnerButton();
+                top += lineHeightSpinner;
+                top += lineHeightSpinner;
+            }
+
+            private void moveMultiSpinnerButton() {
+                //
+                FrameLayout.LayoutParams layoutParams;
+                layoutParams = new FrameLayout.LayoutParams(
+                        button.getWidth(),
+                        button.getHeight()
+                );
+                layoutParams.leftMargin = leftMargin;
+                layoutParams.topMargin = button.getTop() + lineHeightSpinner;
+                button.setLayoutParams(layoutParams);
+                //
+                FrameLayout.LayoutParams layoutParamsSubmit;
+                layoutParamsSubmit = new FrameLayout.LayoutParams(
+                        _mainActivity.findViewById(666).getWidth(),
+                        _mainActivity.findViewById(666).getHeight()
+                );
+                layoutParamsSubmit.leftMargin = leftMargin;
+                layoutParamsSubmit.topMargin = _mainActivity.findViewById(666).getTop() + lineHeightSpinner;
+                _mainActivity.findViewById(666).setLayoutParams(layoutParamsSubmit);
+            }
+        });
+
+        _frameLayout.addView(button);
+    }
+
+    public void addSubmitButton(View.OnClickListener clickListener) {
+        FrameLayout.LayoutParams layoutParams;
+        layoutParams = new FrameLayout.LayoutParams(
+                width,
+                lineHeightButton
+        );
+        layoutParams.leftMargin = leftMargin;
+        layoutParams.topMargin = getNewTop(lineHeightButton);
+
+        final Button button = new Button(_mainActivity);
+        button.setText("Salva");
+        button.setId(666);
+        button.setLayoutParams(layoutParams);
+
+        button.setOnClickListener(clickListener);
+
+        _frameLayout.addView(button);
+    }
+
+    public void addTimePicker() {
+        FrameLayout.LayoutParams layoutParams;
+        layoutParams = new FrameLayout.LayoutParams(
+                width,
+                lineHeightTimePicker
+        );
+        layoutParams.leftMargin = leftMargin;
+        layoutParams.topMargin = getNewTop(lineHeightTimePicker);
+
+        TimePicker textView = new TimePicker(_mainActivity);
+        textView.setLayoutParams(layoutParams);
+
+        _frameLayout.addView(textView);
+    }
+
+    private int getNewTop(int elementHeight) {
+        int returnTop = top + 20;
+        top = top + elementHeight;
+        return returnTop;
+    }
+
+}
---
+++ NutriMondo/src/main/java/com/example/nutrimondo/MainActivity.java
@@ -0,0 +1,168 @@
+package com.example.nutrimondo;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Fragment;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.FrameLayout;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if (savedInstanceState == null) {
+            getFragmentManager().beginTransaction()
+                    .add(R.id.activity_main, new PlaceholderFragment())
+                    .commit();
+        }
+
+        DialogInterface.OnClickListener onClickListener = new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                HttpClient httpclient;
+                HttpPost httppost;
+                List<NameValuePair> nameValuePairs;
+
+                httpclient = new DefaultHttpClient();
+                httppost = new HttpPost("http://www.yiinotes.com/nutrimondo/web/meals");
+                nameValuePairs = new ArrayList<NameValuePair>(2);
+
+                nameValuePairs.add(new BasicNameValuePair("parametro", "valore"));
+
+                try {
+                    httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+                } catch (UnsupportedEncodingException e) {
+                    e.printStackTrace();
+                }
+
+                HttpResponse response;
+                response = null;
+
+                try {
+                    response = httpclient.execute(httppost);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+                try {
+                    InputStream inputStream = response.getEntity().getContent();
+                } catch (IOException e) {
+
+                }
+            }
+        };
+
+        AlertDialog.Builder alert = new AlertDialog.Builder(this)
+                .setTitle("Application created")
+                .setIcon(R.drawable.ic_launcher)
+                .setPositiveButton("Ok", onClickListener);
+        alert.show();
+
+        buildInterface();
+    }
+
+    private void buildInterface() {
+        ArrayAdapter<CharSequence> adapter = getAliments();
+
+        InterfaceBuilder interfaceBuilder = getBuilder();
+        interfaceBuilder.addTextView("Orario del pasto:");
+        interfaceBuilder.addTimePicker();
+        interfaceBuilder.addTextView("Alimento:");
+        interfaceBuilder.addMultiSpinner(adapter);
+        interfaceBuilder.addSubmitButton(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                postData();
+            }
+        });
+    }
+
+    public void postData() {
+        AlertDialog.Builder alert = new AlertDialog.Builder(this);
+        alert.setTitle("Send Post Request");
+        alert.setIcon(R.drawable.ic_launcher);
+        AlertDialog.Builder ok = alert.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+            }
+        });
+        alert.show();
+    }
+
+    private ArrayAdapter<CharSequence> getAliments() {
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.aliments, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        return adapter;
+    }
+
+    private InterfaceBuilder getBuilder() {
+        InterfaceBuilder interfaceBuilder;
+        interfaceBuilder = new InterfaceBuilder(
+                this,
+                (FrameLayout) findViewById(R.id.activity_main)
+        );
+        return interfaceBuilder;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        switch (item.getItemId()) {
+            case R.id.action_settings:
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * A placeholder fragment containing a simple view.
+     */
+    public static class PlaceholderFragment extends Fragment {
+
+        public PlaceholderFragment() {
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+            return rootView;
+        }
+    }
+
+}
