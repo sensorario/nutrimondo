626bd30ffbb0e789c720218ec54bd0ea8fe1b5f9
sensorario <sensorario@gmail.com>
2013-11-17 16:41:10 +0100

    Aggiunto specchio per allodole

--- NutriMondo/src/main/java/com/example/nutrimondo/MainActivity.java
+++ NutriMondo/src/main/java/com/example/nutrimondo/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.nutrimondo;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.Fragment;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.os.StrictMode;
 import android.view.LayoutInflater;
@@ -14,51 +15,18 @@ import android.widget.ArrayAdapter;
 import android.widget.FrameLayout;
 import android.widget.Spinner;
 
-import org.apache.http.HttpResponse;
 import org.apache.http.NameValuePair;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpPost;
-import org.apache.http.entity.ByteArrayEntity;
-import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.message.BasicHeader;
 import org.apache.http.message.BasicNameValuePair;
-import org.apache.http.params.BasicHttpParams;
-import org.apache.http.params.HttpConnectionParams;
-import org.apache.http.params.HttpParams;
-import org.apache.http.protocol.HTTP;
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
 
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
-
-//import android.app.Dialog;
-//import android.app.AlertDialog;
-//import android.content.DialogInterface;
-//import android.os.StrictMode;
-
-//import org.apache.http.HttpResponse;
-//import org.apache.http.NameValuePair;
-//import org.apache.http.client.HttpClient;
-//import org.apache.http.client.entity.UrlEncodedFormEntity;
-//import org.apache.http.client.methods.HttpPost;
-//import org.apache.http.impl.client.DefaultHttpClient;
-//import org.apache.http.message.BasicNameValuePair;
-
-//import java.io.IOException;
-//import java.io.InputStream;
-//import java.io.UnsupportedEncodingException;
-//import java.util.ArrayList;
-//import java.util.Date;
-//import java.util.List;
 
 public class MainActivity extends Activity {
 
@@ -73,62 +41,12 @@ public class MainActivity extends Activity {
                     .commit();
         }
 
-//        DialogInterface.OnClickListener onClickThenAddOnion;
-//        onClickThenAddOnion = onClickThenAddOnion();
-
-//        AlertDialog.Builder alert = new AlertDialog.Builder(this)
-//                .setTitle(new Date().toString())
-//                .setIcon(R.drawable.ic_launcher)
-//                .setPositiveButton("Ok", onClickThenAddOnion);
-//        alert.show();
-
         buildInterface();
     }
 
-//    private DialogInterface.OnClickListener onClickThenAddOnion() {
-//        return new DialogInterface.OnClickListener() {
-//            @Override
-//            public void onClick(DialogInterface dialog, int which) {
-//                HttpClient httpclient;
-//                HttpPost httppost;
-//                List<NameValuePair> nameValuePairs;
-//
-//                StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy
-//                        .Builder()
-//                        .permitAll()
-//                        .build();
-//                StrictMode.setThreadPolicy(policy);
-//
-//                httpclient = new DefaultHttpClient();
-//                httppost = new HttpPost("http://www.yiinotes.com/nutrimondo/web/aggiungi-cipolla");
-//                nameValuePairs = new ArrayList<NameValuePair>(2);
-//                nameValuePairs.add(new BasicNameValuePair("parametro", "valore"));
-//
-//                try {
-//                    httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
-//                } catch (UnsupportedEncodingException e) {
-//                    e.printStackTrace();
-//                }
-//
-//                HttpResponse response;
-//                response = null;
-//
-//                try {
-//                    response = httpclient.execute(httppost);
-//                } catch (IOException e) {
-//                    e.printStackTrace();
-//                }
-//
-//                try {
-//                    InputStream inputStream = response.getEntity().getContent();
-//                } catch (IOException e) {
-//
-//                }
-//
-//                alertMessage("Aggiunta Cipolla");
-//            }
-//        };
-//    }
+    private MainActivity getContext () {
+        return this;
+    }
 
     private void buildInterface() {
         ArrayAdapter<CharSequence> adapter = getAliments();
@@ -153,9 +71,9 @@ public class MainActivity extends Activity {
                 try {
                     List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(2);
                     nameValuePairs.add(new BasicNameValuePair("foods", "" + interfaceBuilder.getFormItems()));
-                    for(int i = interfaceBuilder.getFormItems();i>1;i--) {
+                    for (int i = interfaceBuilder.getFormItems(); i > 1; i--) {
                         Spinner item = (Spinner) findViewById(i);
-                        nameValuePairs.add(new BasicNameValuePair("food_"+i, item.getSelectedItem().toString()));
+                        nameValuePairs.add(new BasicNameValuePair("food_" + i, item.getSelectedItem().toString()));
                     }
                     request.setEntity(new UrlEncodedFormEntity(nameValuePairs));
                     client.execute(request);
@@ -167,17 +85,20 @@ public class MainActivity extends Activity {
                     e.printStackTrace();
                 }
 
+                AlertDialog.Builder alert = new AlertDialog.Builder(getContext());
+                alert.setTitle("Pasto salvato!");
+                alert.setIcon(R.drawable.ic_launcher);
+                alert.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        finish();
+                    }
+                });
+                alert.show();
+
                 finish();
             }
         });
-    }
-
-    private void alertMessage(String title) {
-        AlertDialog.Builder alert = new AlertDialog.Builder(this);
-        alert.setTitle(title);
-        alert.setIcon(R.drawable.ic_launcher);
-        AlertDialog.Builder ok = alert.setPositiveButton("Ok", null);
-        alert.show();
     }
 
     private ArrayAdapter<CharSequence> getAliments() {
@@ -193,34 +114,28 @@ public class MainActivity extends Activity {
                 this,
                 (FrameLayout) findViewById(R.id.activity_main)
         );
+
         return interfaceBuilder;
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
+
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         switch (item.getItemId()) {
             case R.id.action_settings:
                 return true;
         }
+
         return super.onOptionsItemSelected(item);
     }
 
-    /**
-     * A placeholder fragment containing a simple view.
-     */
     public static class PlaceholderFragment extends Fragment {
-
         public PlaceholderFragment() {
         }
 
--- NutriMondo/src/main/java/com/example/nutrimondo/SplashActivity.java
+++ NutriMondo/src/main/java/com/example/nutrimondo/SplashActivity.java
@@ -1,6 +1,7 @@
 package com.example.nutrimondo;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -18,6 +19,14 @@ public class SplashActivity extends Activity {
     public void openMainActivity(View view) {
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
+    }
+
+    public void openHistory(View view) {
+        AlertDialog.Builder alert = new AlertDialog.Builder(this);
+        alert.setTitle("Da fare ...");
+        alert.setIcon(R.drawable.ic_launcher);
+        alert.setPositiveButton("Ok", null);
+        alert.show();
     }
 
 }
