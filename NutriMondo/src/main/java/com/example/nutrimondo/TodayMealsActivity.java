69837665a9056f1892c3686b527820640bad8acc
sensorario <sensorario@gmail.com>
2013-11-26 06:13:30 +0100

    initial commit

--- NutriMondo/src/main/java/com/example/nutrimondo/TodayMealsActivity.java
+++ NutriMondo/src/main/java/com/example/nutrimondo/TodayMealsActivity.java
@@ -7,6 +7,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.webkit.WebView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 /**
  * Created by simonegentili on 18/11/13.
@@ -17,8 +19,26 @@ public class TodayMealsActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_today);
 
-        WebView webView = (WebView) findViewById(R.id.web_view);
-        webView.loadUrl("http://www.yiinotes.com/nutrimondo/web/meals/today");
+        String[] strings;
+        strings = new String[]{
+                "Uno",
+                "Due",
+                "Tre",
+                "Quattro",
+                "Cinque",
+                "Sei",
+                "Sette",
+                "Otto"
+        };
+        final TodayMealsActivity context = this;
+        final int simple_list_item_1 = android.R.layout.simple_list_item_1;
+        ArrayAdapter adapter = new ArrayAdapter<String>(
+                context,
+                simple_list_item_1,
+                strings
+        );
+        ListView listView = (ListView) findViewById(R.id.listView);
+        listView.setAdapter(adapter);
 
         getActionBar().setDisplayHomeAsUpEnabled(true);
     }
